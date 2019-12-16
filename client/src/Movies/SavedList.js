f27e1bb8a3cecf31546f30255d4d22d343d07a1c
Vincent Sanders <vincesanders@hotmail.com>
2019-12-16 16:07:46 -0600

    STRETCH: changed SavedList so that saved movies are now NavLinks and also slightly altered their style.

--- client/src/Movies/SavedList.js
+++ client/src/Movies/SavedList.js
@@ -1,11 +1,12 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+import { Link, NavLink } from 'react-router-dom';
 
 const SavedList = props => (
   <div className="saved-list">
     <h3>Saved Movies:</h3>
     {props.list.map(movie => (
-      <span className="saved-movie">{movie.title}</span>
+      // <span className="saved-movie">{movie.title}</span>
+      <NavLink className="saved-movie" to={`/movies/${movie.id}`}>{movie.title}</NavLink>
     ))}
     <Link to='/' className="home-button">Home</Link>
   </div>
